12cf3370cb6737707f54d63d4a2aa9a98da7b76e
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-05-24 19:26:27 -0400

    fixed indentation of quiz js

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -18,33 +18,22 @@ function Quiz(props){
       }
       
     },[answersData])
- 
-
-
-
    useEffect(()=>{
-    
+  
       if(answersData && props.victory){
     answersData.map(x=>x.clicked && x.correct ? props.setCorrectPicked(prev=> prev+1) :"" )
        }
          },[props.victory])
-   
-
-  
- 
-
   function isClicked(id){
     setAnswersData(prev=>{
      return prev.map(x=>{
        return x.id===id ? {...x, clicked:true} : {...x, clicked:false}
      })
     })
-    
+ 
      return setChecker(true)
     }
    
- 
-
  const answerMapper=(answersData.map(y=>{
  return( <Answers 
  key={y.id}
@@ -53,27 +42,17 @@ function Quiz(props){
  isClicked={isClicked}
  victory={props.victory}
 setCorrectPicked={props.setCorrectPicked}
- 
  />)
 
  }))
-
-
-
-
-
-
 return(
     <div className="quiz-parent">
-       
-      <h2 className="question-text">{decode(props.question)}  </h2>
+        <h2 className="question-text">{decode(props.question)}  </h2>
       <div className="answers">
       {answerMapper}
       </div>
-        
+    
     </div>
 )
-
 }
-
 export default Quiz;
